0df485ec98cd7a2280f903f8d90cec8aa95592f3
Oliver Kisielius <oliver.kisielius@gmail.com>
2023-09-15 18:25:52 -0400

    Renamed the file but kept editing the old file, fixing

--- random_pixel_tree.py
+++ random_pixel_tree.py
@@ -1,7 +1,9 @@
 """
-A script that makes a two-color image depicting a random tree of pixels.
-The tree grows in random directions but the branches avoid touching each other.
-... It looks like a bowl of noodles if you choose noodle colors.
+A script that makes a two-color image depicting a random tree of pixels.  The
+tree grows in random directions as far as it can without the branches ever
+touching each other.
+
+... It looks like a bowl of noodles. If you choose noodle colors.
 """
 from absl import app
 from absl import flags
@@ -30,43 +32,6 @@ _FG_COLOR_FLAG = flags.DEFINE_string(
 
 
 T = TypeVar("T")
-
-
-# When you implement Union Find but then realize you don't need it, but you
-# don't want to delete your implementation because it's your fave data structure
-# class UnionFind(Generic[T]):
-
-#     def __init__(self) -> None:
-#         self._parent = {}
-#         # Size is tricky because only the root node sizes are accurate.
-#         self._size = {}
-
-#     def _add_if_missing(self, elem: T) -> None:
-#         if elem not in self._parent:
-#             self._parent[elem] = elem
-#             self._size[elem] = 1
-
-#     def find(self, elem: T) -> T:
-#         """Returns the root of the tree containing elem."""
-#         self._add_if_missing(elem)
-#         if self._parent[elem] == elem:
-#             return elem
-#         # Recursion with path compression
-#         self._parent[elem] = self.find(self._parent[elem])
-#         return self._parent[elem]
-
-#     def union(self, a: T, b: T) -> None:
-#         """Merges the trees containing a and b."""
-#         a = self.find(a)
-#         b = self.find(b)
-#         if b == a:
-#             return
-#         if self._size[a] > self._size[b]:
-#             b, a = a, b
-#         # b is bigger so we hang a from b
-#         self._parent[a] = b
-#         self._size[b] += self._size[a]
-#         del self._size[a]
 
 
 def _neighbors(coord: tuple[int, int], width: int, height: int) -> list[tuple[int, int]]:
